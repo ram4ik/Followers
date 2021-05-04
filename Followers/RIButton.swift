2a3a0c12d6378562278ae62cf2fd7d097d0c17d0
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-04 22:30:31 +0300

    button & textField

---
+++ Followers/RIButton.swift
@@ -0,0 +1,35 @@
+//
+//  RIButton.swift
+//  Followers
+//
+//  Created by ramil on 04.05.2021.
+//
+
+import UIKit
+
+class RIButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}

37a7ba6d2d24748eae50057753d810d035c168c0
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-05 12:14:07 +0300

    models

---
+++ Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  Followers
+//
+//  Created by ramil on 05.05.2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}

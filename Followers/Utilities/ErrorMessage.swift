a2a91b00bd4fe02312ddf22932cf06f8c44c9d46
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-05-05 13:31:09 +0300

    error messages

---
+++ Followers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  Followers
+//
+//  Created by ramil on 05.05.2021.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case invalidUserName = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server is invalid. Please try again."
+}
